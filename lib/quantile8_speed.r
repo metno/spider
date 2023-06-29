c477220339acdbd18b8ba359a8ad3370f3d879d1
klinogrid <klinogrid@ppi-r8login-b1.int.met.no>
2023-06-29 13:33:38 +0000

    quantile8_speed can now manage some exceptions

--- lib/quantile8_speed.r
+++ lib/quantile8_speed.r
@@ -1,12 +1,15 @@
 quantile8_speed <- function(x, probs = c(0.1, 0.9), na.rm = F) {
 
-  if (na.rm) x <- x[!is.na(x)]
+  if ( length( ix <- which(!is.na(x))) == 0) return( rep(NA,length(probs)))
 
+  if (na.rm) x <- x[ix]
   n <- length(x)
   index <- 1/3 + (n + 1/3) * probs
 
   lo    <- floor(index)
   hi    <- ceiling(index)
+  if ( length( ix <- which(lo<1)) > 0) lo[ix] <- 1
+  if ( length( ix <- which(hi>n)) > 0) hi[ix] <- n
 
   x  <- sort(x, partial = unique(c(lo, hi)))
   qs <- x[lo]
